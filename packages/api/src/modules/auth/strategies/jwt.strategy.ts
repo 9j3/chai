34f18e90d98c9f90cb9bf9d9477f5b6ffd91858f
Laurin Birchler <58566423+9j3@users.noreply.github.com>
2022-08-14 20:06:48 +0200

    added jwt secret into Strategy

--- packages/api/src/modules/auth/strategies/jwt.strategy.ts
+++ packages/api/src/modules/auth/strategies/jwt.strategy.ts
@@ -20,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: '<SECRET KEY>',
+      secretOrKey: 'secret',
       signOptions: {
         expiresIn: '5m',
       },
