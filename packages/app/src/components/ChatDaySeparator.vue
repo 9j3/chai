0ce7ad94fb01c746e7ec1a4b4beca433d1e91d41
Dario Bösch <dario.boesch@gmail.com>
2022-08-14 12:32:08 +0200

    add components for chat view

---
+++ packages/app/src/components/ChatDaySeparator.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+    dateString: String
+})
+</script>
+
+<template>
+    <div class="text-center my-5">
+        <hr class="-mb-3">
+        <span class="text-xs text-slate-300 font-medium bg-white px-3 -mt-3">{{ dateString }}</span>
+    </div>
+</template>
