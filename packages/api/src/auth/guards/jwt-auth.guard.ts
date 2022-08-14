30f0d7ef65906369f6af446ae3bea266552c840b
Laurin Birchler <58566423+9j3@users.noreply.github.com>
2022-08-14 15:07:27 +0200

    Enriched JWTAuthGuard with implementations of canActivate

--- packages/api/src/auth/guards/jwt-auth.guard.ts
+++ packages/api/src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+
+  /**
+     * 
+     * @param context
+     */
+  canActivate(context: ExecutionContext) {
+    // TODO: Add custom authentication logic here
+    return super.canActivate(context);
+  }
+
+  /**
+     * 
+     * @param err
+     * @param user
+     * @param info
+     */
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
