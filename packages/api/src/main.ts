7c098e8ad5bae55f6b91cb1138d52bae504bad4f
Laurin Birchler <58566423+9j3@users.noreply.github.com>
2022-08-22 23:25:29 +0200

    fixed merge conflicts

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -6,6 +6,8 @@ declare const module: any
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule)
 
+  app.enableCors()
+
   const port = parseInt(process.env.PORT) || 3000
   const host = process.env.HOST || '127.0.0.1'
 
--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,5 +1,17 @@
-<script setup></script>
+<script setup>
+import { Nav, Alert } from '@/components';
+import { useAuthStore } from '@/stores';
+const authStore = useAuthStore();
+</script>
 
-<template />
-
-<style scoped></style>
+<template>
+  <div
+    class="app-container"
+    :class="authStore.user && 'bg-light'"
+  >
+    <Alert />
+    <div class="">
+      <router-view />
+    </div>
+  </div>
+</template>
