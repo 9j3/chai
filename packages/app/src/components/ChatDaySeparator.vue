2adb623a40c15f545981597ff59a9b034d7e5e37
Dario Bösch <dario.boesch@gmail.com>
2022-08-23 17:42:23 +0200

    format components
    apply eslint rules
    
    # Conflicts:
    #	packages/app/src/components/CreateAccount.vue

--- packages/app/src/components/ChatDaySeparator.vue
+++ packages/app/src/components/ChatDaySeparator.vue
@@ -5,8 +5,8 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="text-center my-5">
-        <hr class="-mb-3">
-        <span class="text-xs text-slate-300 font-medium bg-white px-3 -mt-3">{{ dateString }}</span>
-    </div>
+  <div class="text-center my-5">
+    <hr class="-mb-3">
+    <span class="text-xs text-slate-300 font-medium bg-white px-3 -mt-3">{{ dateString }}</span>
+  </div>
 </template>
--- packages/app/src/components/LoginWindow.vue
+++ packages/app/src/components/LoginWindow.vue
@@ -13,39 +13,52 @@ function createAccount() {
 </script>
 
 <template>
-    <div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
-        <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-            <h1 class="font-bold text-center text-2xl mb-5">Chai</h1>
-            <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
-                <div class="px-5 py-7">
-                    <label class="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
-                    <input type="text" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
-                    <label class="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
-                    <input type="password" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
-                    <button type="button" @click="loginUser"
-                        class="transition duration-200 bg-primary hover:bg-primary-hover focus:bg-primary-hover text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
-                        <span class="inline-block mr-2">Login</span>
-                    </button>
-                </div>
-                <div class="py-5">
-                    <div class="grid grid-cols-2 gap-1">
-                        <div class="text-center sm:text-left whitespace-nowrap">
-                            <button
-                                class="transition duration-200 mx-8 px-2 py-2 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200"
-                                @click="resetPassword">
-                                <span class="inline-block ml-1">Forgot Password</span>
-                            </button>
-                        </div>
-                        <div class="text-center sm:text-right whitespace-nowrap">
-                            <button
-                                class="transition duration-200 mx-8 px-2 py-2 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200"
-                                @click="createAccount">
-                                <span class="inline-block ml-1">Create Account</span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+    <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+      <h1 class="font-bold text-center text-2xl mb-5">
+        Chai
+      </h1>
+      <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+        <div class="px-5 py-7">
+          <label class="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
+          <input
+            type="text"
+            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+          >
+          <label class="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
+          <input
+            type="password"
+            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+          >
+          <button
+            type="button"
+            class="transition duration-200 bg-primary hover:bg-primary-hover focus:bg-primary-hover text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+            @click="loginUser"
+          >
+            <span class="inline-block mr-2">Login</span>
+          </button>
         </div>
+        <div class="py-5">
+          <div class="grid grid-cols-2 gap-1">
+            <div class="text-center sm:text-left whitespace-nowrap">
+              <button
+                class="transition duration-200 mx-8 px-2 py-2 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200"
+                @click="resetPassword"
+              >
+                <span class="inline-block ml-1">Forgot Password</span>
+              </button>
+            </div>
+            <div class="text-center sm:text-right whitespace-nowrap">
+              <button
+                class="transition duration-200 mx-8 px-2 py-2 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200"
+                @click="createAccount"
+              >
+                <span class="inline-block ml-1">Create Account</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
