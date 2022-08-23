5d4d831d69d2bb19db3d30fe6dd6dc7c98fbc3a8
Laurin Birchler <58566423+9j3@users.noreply.github.com>
2022-08-23 12:47:18 +0200

    added event gateway

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 
 import { UsersModule } from './modules/users/users.module'
 import { AuthenticationModule } from './modules/auth/auth.module'
+import { EventsModule } from './events/events.module'
 
 @Module({
-  imports: [UsersModule, AuthenticationModule],
+  imports: [UsersModule, EventsModule, AuthenticationModule],
   controllers: [],
   providers: []
 })
