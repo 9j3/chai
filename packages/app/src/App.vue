0dfc102286dab99f4f9e296588255e6d000ab316
Laurin Birchler <58566423+9j3@users.noreply.github.com>
2022-08-23 13:04:16 +0200

    added chat repository

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
 import { Nav, Alert } from '@/components';
 import { useAuthStore } from '@/stores';
+import { useSocketIO } from "@/socket";
+
 const authStore = useAuthStore();
+const { socket } = useSocketIO()
+
+socket.on('welcome', () => { console.log('welcome') })
+
 </script>
 
 <template>
