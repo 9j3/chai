9ebe26478304532ab1d093d5ab8c6fa985e5c27b
Laurin Birchler <58566423+9j3@users.noreply.github.com>
2022-08-28 23:55:58 +0200

    removed airbnb config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,33 @@
-const DOMGlobals = ['window', 'document']
-const NodeGlobals = ['module', 'require']
+const DOMGlobals = ['window', 'document'];
+const NodeGlobals = ['module', 'require'];
 
 // eslint-disable-next-line no-restricted-globals
 module.exports = {
   parserOptions: {
-    "ecmaVersion": "latest"
+    ecmaVersion: 'latest',
   },
   rules: {
     'no-debugger': 'error',
     'no-unused-vars': ['error', { varsIgnorePattern: '.*', args: 'none' }],
     'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
-    'no-restricted-syntax': ['error', 'ObjectPattern > RestElement']
+    'no-restricted-syntax': ['error', 'ObjectPattern > RestElement'],
   },
   overrides: [
     {
       files: ['packages/app/**'],
-      extends: [
-        'plugin:vue/vue3-recommended',
-        '@vue/eslint-config-airbnb'
-      ],
+      extends: ['plugin:vue/vue3-recommended'],
       rules: {
         'no-restricted-globals': ['error', ...DOMGlobals],
-          "import/extensions": [
-            "error",
-            "ignorePackages",
-            {
-              "js": "never",
-              "vue": "never"
-            }
-          ]
-      }
+        'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
+      },
     },
     {
       parser: '@typescript-eslint/parser',
       files: ['packages/api/**'],
       rules: {
         'no-restricted-globals': ['error', ...NodeGlobals],
-        'no-restricted-syntax': 'off'
-      }
-    }
-  ]
-}
+        'no-restricted-syntax': 'off',
+      },
+    },
+  ],
+};
