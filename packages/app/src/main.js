c79a2fc4bf7a580d8df55d91d08bb803f429c672
Laurin Birchler <58566423+9j3@users.noreply.github.com>
2022-09-02 08:53:35 +0200

    simplified complete app due to complexity

--- packages/app/src/main.js
+++ packages/app/src/main.js
@@ -3,19 +3,9 @@ import { createPinia } from 'pinia';
 import './index.css';
 
 import App from './App.vue';
-import { router } from './router';
-import socket from '@/plugins/socket';
-import { setAppInstance } from '@/socket';
 
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(router);
-
-app.use(socket, {
-  connection: 'http://localhost:3000/chat',
-});
 
 app.mount('#app');
-
-setAppInstance(app);
