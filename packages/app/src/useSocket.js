bc4d0dc5fad1d87f0a6e04d7939674df9ae62d90
Laurin Birchler <58566423+9j3@users.noreply.github.com>
2022-09-09 19:28:31 +0200

    migrated to nest cli

--- packages/app/src/useSocket.js
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-export const useSocket = () => {
-  return io('http://localhost:3000/chat', {
-    autoConnect: false,
-    transports: ['websocket'],
-  });
-};
